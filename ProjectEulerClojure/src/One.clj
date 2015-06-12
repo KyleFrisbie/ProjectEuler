8636a6f7e405d409331ec4331f4ecec60fe255b7
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2015-06-12 16:50:09 -0600

    removed some files

--- ProjectEulerClojure/src/One.clj
+++
@@ -1,9 +0,0 @@
-(defn one-sum [n]
-  (cond (= 1000 n)
-    0
-    (or (= 0 (mod n 3))(= 0 (mod n 5)))
-;    (do (println n) (one-sum (inc n)))
-    (+ n (one-sum (inc n)))
-    :else (one-sum (inc n))))
-
-(println (one-sum 1))
--- ProjectEulerClojure/src/Two.clj
+++
@@ -1,6 +0,0 @@
-(defn fib-sum [n m]
-  (if (> 4000000 (+ n m))
-    0
-    (+ n (fib-sum (m (+ n m))))))
-
-(println (fib-sum 1 2))
